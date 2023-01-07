8a33b07c38bbde9f4813b027312de9f422f8569f
aditya <aditya2@mygetepay.com>
2023-01-07 18:41:07 +0530

    second commit

--- base/models.py
+++ base/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank=True) #cascade is used so that if a user has deleted itself all the data related to the user will be deleted
     title = models.CharField(max_length=200, null=True, blank=True)
 
     description = models.TextField(null=True, blank=True)
--- base/views.py
+++ base/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Task
 from django.urls import reverse_lazy
-
+from django.contrib.auth.views import LoginView
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks'
+)
+
 
 class TaskList(ListView):
     #will look for html with name model_list.html
@@ -24,3 +34,14 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__' 
     success_url = reverse_lazy('tasks')  #this is used to redirect after submitting the form
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task   #look for html file with model_confirm_delete.html
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
